ff00818d76dccb734ffde95a707f0c4cb3599db6
MohamedSonata <96669613+MohamedSonata@users.noreply.github.com>
2022-02-02 19:45:43 +0200

    new edit env

--- config/plugins.js
+++ config/plugins.js
@@ -3,11 +3,11 @@ module.exports = {
     config: {
       provider: "strapi-provider-upload-dos",
       providerOptions: {
-        key: process.env.example.DO_SPACE_ACCESS_KEY,
-        secret: process.env.example.DO_SPACE_SECRET_KEY,
-        endpoint: process.env.example.DO_SPACE_ENDPOINT,
-        space: process.env.example.DO_SPACE_BUCKET,
-        directory: process.env.example.DO_SPACE_DIRECTORY
+        key: process.env.DO_SPACE_ACCESS_KEY,
+        secret: process.env.DO_SPACE_SECRET_KEY,
+        endpoint: process.env.DO_SPACE_ENDPOINT,
+        space: process.env.DO_SPACE_BUCKET,
+        directory: process.env.DO_SPACE_DIRECTORY
       },
     },
   },
