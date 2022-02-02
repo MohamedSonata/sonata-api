65bce71420f0d36869db016aca5c5f40a1bc25af
MohamedSonata <96669613+MohamedSonata@users.noreply.github.com>
2022-02-02 04:52:43 +0200

    space

--- extensions/upload/config/settings.js
+++ extensions/upload/config/settings.js
@@ -1,11 +1,11 @@
 module.exports = {
     provider: "do",
     providerOptions: {
-      key: process.env.DO_SPACE_ACCESS_KEY,
-      secret: process.env.DO_SPACE_SECRET_KEY,
-      endpoint: process.env.DO_SPACE_ENDPOINT,
-      space: process.env.DO_SPACE_BUCKET,
-      directory: process.env.DO_SPACE_DIRECTORY,
-      cdn: process.env.DO_SPACE_CDN,
+      key: 'ZG4NK7WVFZF6XM3D4O37',
+      secret: 'FVXrL/cBVJJmu/qOmo95BQh0lQeGyv4UNeBjy1g9Vhw',
+      endpoint: 'fra1.digitaloceanspaces.com',
+      space: 'sonataspace',
+      directory: '/assets',
+      
     }
   }
