9927ea965f259bb4721159ce162063ddc872ebec
MohamedSonata <96669613+MohamedSonata@users.noreply.github.com>
2022-02-02 04:11:11 +0200

    keys

--- config/server.js
+++ config/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: ['dt3y1/02KoyaSk6cmdO0qg==','mb81hKDJsGXoyY/5J2Esgg=='],
   },
 });
