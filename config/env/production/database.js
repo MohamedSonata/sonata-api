0aa4784af2ec51ed35724f706544e336730c13ae
MohamedSonata <96669613+MohamedSonata@users.noreply.github.com>
2022-02-02 03:38:45 +0200

    new

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -8,11 +8,11 @@ module.exports = () => ({
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: 'db-postgresql-fra1-40846-do-user-10645723-0.b.db.ondigitalocean.com',
-        port: '25060',
-        database: 'defaultdb',
-        username: 'doadmin',
-        password: 'HRHRJxWC2tKTEo4u',
+        host: config.host,
+        port: config.port,
+        database: config.database,
+        username: config.user,
+        password: config.password,
         ssl: {
           rejectUnauthorized: false,
         },
