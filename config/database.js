9e990f887634dc190a9208287d0367497683e3c1
MohamedSonata <96669613+MohamedSonata@users.noreply.github.com>
2022-02-04 01:36:13 +0200

    new postgres data

--- config/database.js
+++ config/database.js
@@ -1,11 +1,19 @@
-const path = require('path');
-
 module.exports = ({ env }) => ({
-  connection: {
-    client: 'postgres',
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      client: 'postgres',
+      connection: {
+        host: env('DATABASE_HOST', 'db-postgresql-fra1-40846-do-user-10645723-0.b.db.ondigitalocean.com'),
+        port: env.int('DATABASE_PORT', 25060),
+        database: env('DATABASE_NAME', 'defaultdb'),
+        user: env('DATABASE_USERNAME', 'doadmin'),
+        password: env('DATABASE_PASSWORD', 'HRHRJxWC2tKTEo4u'),
+        schema: env('DATABASE_SCHEMA', 'public'), // Not required
+        ssl: {
+          rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+        },
+      },
+      debug: false,
     },
-    useNullAsDefault: true,
-  },
-});
+  });
+   
+ 
